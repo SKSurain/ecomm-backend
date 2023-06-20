ddd7ab21e80f1fce43d3eb6f640ccc284930c8a4
Surain <surain.wira@gmail.com>
2023-06-20 15:52:58 +0800

    First Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+const userRoutes = require("./routes/users")
+const authRoutes = require("./routes/auth")
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => { console.log("Connection Success") })
+    .catch((e) => { console.log(e) })
+
+app.use(express.json());
+app.use("api/auth", authRoutes)
+app.use("/api/users", userRoutes);
+
+
+app.listen(process.env.PORT_NUMBER || 5000, () => {
+    console.log("Backend server is running")
+})
