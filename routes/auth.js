4623c3ae7445b1b3a6f8be1a86379b049f746bcd
Surain <surain.wira@gmail.com>
2023-06-22 01:21:18 +0800

    ADDED JWT and cryptojs npm as security features

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const CryptoJs = require("crypto-js");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 //Register
 router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: CryptoJs.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
     });
 
     try {
@@ -18,6 +19,40 @@ router.post("/register", async (req, res) => {
         res.status(500).json(err)
     }
 })
+
+
+//Login
+router.post("/login", async (req, res) => {
+
+    try {
+        const user = await User.findOne({ username: req.body.username });
+
+        !user && res.status(401).json("Wrong Credentials")
+
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.Pass_Sec
+        );
+        const dbpassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        dbpassword !== req.body.password && res.status(401).json("Wrong Credentials");
+
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin,
+        },
+            process.env.JWT_SEC,
+            { expiresIn: "3d" })
+
+        const { password, ...others } = user._doc;
+
+        res.status(201).json({ ...others, accessToken })
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 
 
 module.exports = router; 
