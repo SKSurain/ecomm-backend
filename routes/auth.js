38dcdcce4e82bcaf593ef393d7292778ab88ea59
Surain <surain.wira@gmail.com>
2023-06-21 14:41:33 +0800

    used crypto js to save registered user's passwords

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,18 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJs = require("crypto-js");
 //Register
 router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: CryptoJs.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
     });
 
     try {
         const savedUser = await newUser.save();
         res.status(201).json(savedUser)
+
     }
     catch (err) {
         res.status(500).json(err)
