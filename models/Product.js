462d2ebe3f31df6355bf545c65603b7248126064
Surain <surain.wira@gmail.com>
2023-06-23 06:23:36 +0800

    added a few properties to product model

--- models/Product.js
+++ models/Product.js
@@ -23,11 +23,15 @@ const ProductSchema = new mongoose.Schema(
             type: String,
         },
         color: {
-            type: String,
+            type: Array,
         },
         price: {
             type: Number,
             required: true
+        },
+        inStock: {
+            type: Boolean,
+            default: true,
         }
     }, { timestamps: true }
 )
