b23ef263d5850b80e836537800f9bb59cf958005
Surain <surain.wira@gmail.com>
2023-06-25 11:03:29 +0800

    updated position of cors to make it inclusive

--- index.js
+++ index.js
@@ -17,13 +17,14 @@ mongoose
     .then(() => { console.log("Connection Success") })
     .catch((e) => { console.log(e) })
 
+app.use(cors());
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/order", orderRoutes);
-app.use(cors());
 app.use("/api/checkout", stripeRoutes);
 
 app.listen(process.env.PORT_NUMBER || 5000, () => {
