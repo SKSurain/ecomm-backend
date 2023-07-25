dfbc8a5ff84e6ef2a92a343e6d45aa2314fd5d7e
Surain <surain.wira@gmail.com>
2023-07-25 17:03:20 +0800

    errors now more detailed

--- routes/cart.js
+++ routes/cart.js
@@ -26,7 +26,7 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
         res.status(200).json(updateCart)
 
     } catch (err) {
-        res.status(500).json(err);
+        res.status(500).json({ message: err.message })
     }
 });
 
